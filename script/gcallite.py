20218527f43ab9f474371dda46fd341ff9edcf25
petershaw23 <michael.budjan@gmail.com>
2019-07-23 12:05:16 +0200

    push

--- script/gcallite.py
+++ script/gcallite.py
@@ -1,28 +1,18 @@
+#only works with prior oauth via google api. need files "token.pickle" and/or (?) credentials.json in same dir! read https://developers.google.com/calendar/quickstart/python for info
 from datetime import datetime
 from datetime import timedelta
 import pickle
 import os.path
 from googleapiclient.discovery import build
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-heuteGeb = ''
 creds = None
 # The file token.pickle stores the user's access and refresh tokens, and is
 # created automatically when the authorization flow completes for the first
-# time.
+# time. 
+
 if os.path.exists('token.pickle'):
      with open('token.pickle', 'rb') as token:
          creds = pickle.load(token)
- # If there are no (valid) credentials available, let the user log in.
-# if not creds or not creds.valid:
-#        if creds and creds.expired and creds.refresh_token:
-#            creds.refresh(Request())
-#        else:
-#            flow = InstalledAppFlow.from_client_secrets_file(
-#                'credentials.json', SCOPES)
-#            creds = flow.run_local_server(port=0)
-#        # Save the credentials for the next run
-#        with open('token.pickle', 'wb') as token:
-#            pickle.dump(creds, token)
 
 service = build('calendar', 'v3', credentials=creds)
 
