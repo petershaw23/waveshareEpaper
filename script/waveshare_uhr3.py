fe187bab0c0c0515416ced679779585617f0fd40
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-07 20:50:26 +0200

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -79,7 +79,8 @@ trackid = subprocess.Popen("curl 192.168.0.241/api/v1/getstate", stdout=subproce
 if trackid.returncode != 0: #if offline
    artist = ' '
    trackname = ' '
-   trackIDString = '        Volumio Offline'
+   #trackIDString = '        Volumio Offline' # placeholder for test 
+   trackIDString = ' '
 else:
    trackname = outputRAW.decode().split('\"')[9]
    artist = outputRAW.decode().split('\"')[13]
