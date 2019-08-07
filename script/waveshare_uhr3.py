193b52c56b7bee188cd42629cb1f482a391e2d69
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-07 17:11:10 +0200

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -111,8 +111,8 @@ def main():
         draw = ImageDraw.Draw(image)
         #draw.rectangle((0, 0, 264, 49), fill = 0) #rectangle behind bdays and date
         draw.rectangle((0, 48, 264, 71), fill = 0) #rectangle behind track ID
-        draw.text((0, -7), Datum, font = fontXL, fill = 0)              # Date
-        draw.text((75, -6), gebStringNext, font = fontL, fill = 0)     # bday1
+        draw.text((0, -7), str(Datum)+str(gebStringNext), font = fontXL, fill = 0)              # Date + next bday
+        #draw.text((75, -6), gebStringNext, font = fontL, fill = 0)     # bday1 old version, different size than date
         draw.line((0, 24, 264, 24), fill = 0)
         draw.text((0, 23), gebStringUeberNext, font = fontS, fill = 0) #bday2
         #draw.line((0, 48, 264, 48), fill = 0) # black line below bday 2
