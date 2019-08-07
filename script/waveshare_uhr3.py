4915077e176ec5aaf7eb8d48f3915b9ee8b21dd4
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-07 16:23:45 +0200

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -109,12 +109,13 @@ def main():
         image = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)
         #Object image on which we will draw
         draw = ImageDraw.Draw(image)
+        draw.rectangle((0, 20, 264, 45), fill = 0)
         draw.text((0, -7), Datum, font = fontXL, fill = 1)              # Date
         draw.text((98, -7), gebStringNext, font = fontL, fill = 1)     # bday1
         draw.text((0, 23), gebStringUeberNext, font = fontS, fill = 0) #bday2
         draw.line((0, 50, 264, 50), fill = 1)
         #draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-        draw.rectangle((0, 20, 264, 45), fill = 0)
+        
         #draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
         draw.text((0, 49), trackIDString, font = fontM, fill = 0)       # volumio track ID
         draw.line((0, 78, 264, 78), fill = 0)
