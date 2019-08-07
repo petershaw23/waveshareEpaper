34e815d190c994fe413ebcd3e2408d14eb8169e2
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-07 20:56:06 +0200

    Update exec.sh

--- script/exec.sh
+++ script/exec.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# u can call this file in crontab to execute the main script
+# like so: 
+# crontab -e 
+# * * * * * /home/pi/script/waveshareEpaper/script/exec.sh >> /home/pi/script/waveshareEpaper/clocklog2.txt 2>&1
+
 export DISPLAY=:0.0
 source /home/pi/.profile
 python3 /home/pi/script/waveshareEpaper/script/waveshare_uhr3.py
