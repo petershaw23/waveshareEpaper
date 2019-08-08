6948ec66fab66b0b7b16941f7ff9c6d3ac5f605c
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-08 11:37:30 +0200

    Update maps.py

--- script/maps.py
+++ script/maps.py
@@ -5,12 +5,6 @@ import credentialsmaps
 maps_key = credentialsmaps.maps_key
 gmaps = googlemaps.Client(key=maps_key)
 
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
 # Request directions via public transit
 now = datetime.now()
 directions_result = gmaps.directions("Sydney Town Hall",
