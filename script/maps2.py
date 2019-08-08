2fb61827b47c4e67a2430e82d86db98e010c35eb
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-08 14:57:59 +0200

    Update maps2.py

--- script/maps2.py
+++ script/maps2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import json
 import requests
+from datetime import datetime
+from datetime import timedelta
 import credentialsmaps # imports local file credentialmaps.py with google maps api key
+
 maps_key = credentialsmaps.maps_key
 url = "https://maps.googleapis.com/maps/api/directions/json?origin=Hauptbahnhof+Mannheim&destination=Hauptbahnhof+Heidelberg&language=de&mode=transit&key=" + maps_key
 data = requests.get(url=url)
@@ -10,3 +13,5 @@ jsonobj = json.loads(data.content)
 
 depart = (jsonobj["routes"][0]["legs"][0]["arrival_time"]["text"])
 print (str('naechster zug: ')+str(depart))
+delta = (datetime.now() - timedelta(depart))
+print (delta)
