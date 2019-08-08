b10ec1e11493c5c192e6099a9d8a05d108cda97e
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-08 13:38:27 +0200

    Update maps2.py

--- script/maps2.py
+++ script/maps2.py
@@ -2,10 +2,11 @@ import json
 import requests
 import credentialsmaps # imports local file credentialmaps.py with google maps api key
 maps_key = credentialsmaps.maps_key
-url = "https://maps.googleapis.com/maps/api/directions/json?origin=Brooklyn&destination=Queens&mode=transit&key=" + maps_key
+url = "https://maps.googleapis.com/maps/api/directions/json?origin=Hauptbahnhof+Mannheim&destination=Hauptbahnhof+Heidelberg&language=de&mode=transit&key=" + maps_key
 data = requests.get(url=url)
 binary = data.content
 output = json.loads(binary)
 
 # test to see if the request was valid
 print (output['status'])
+print (output['departure_time']
