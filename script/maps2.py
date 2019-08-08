39628b22b4a0e0593b491c35e3ae618dae243a78
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-08 14:12:26 +0200

    Update maps2.py

--- script/maps2.py
+++ script/maps2.py
@@ -5,7 +5,7 @@ maps_key = credentialsmaps.maps_key
 url = "https://maps.googleapis.com/maps/api/directions/json?origin=Hauptbahnhof+Mannheim&destination=Hauptbahnhof+Heidelberg&language=de&mode=transit&key=" + maps_key
 data = requests.get(url=url)
 binary = data.content
-output = json.loads(binary)
+output = json.dumps(binary)
 
 # test to see if the request was valid
 
