a6d434c3bab7976042739d805c761dbfec2166df
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-09 16:08:18 +0200

    Update maps2.py

--- script/maps2.py
+++ script/maps2.py
@@ -25,9 +25,9 @@ print (delta_minute)
 if delta_minute < 15 > 12:
     print ('das wird eng!')
 elif delta_minute == 12:
-    print ('jetzt losrennen!)
+    print ('jetzt losrennen!')
 elif delta_minute < 12:
-    print ('den kriegst du nicht mehr!)
+    print ('den kriegst du nicht mehr!')
 #delta_uncorrect = depart_conv - datetime.now() 
 #print (delta_uncorrect)
 
