b80895179b66ceee1e5e057d4d51280d63a1282e
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-12 12:01:07 +0200

    Update button_test.py

--- script/button_test.py
+++ script/button_test.py
@@ -1,44 +1,26 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-func = GPIO.gpio_function(01)
-func = GPIO.gpio_function(2)
-func = GPIO.gpio_function(3)
-func = GPIO.gpio_function(4)
-func = GPIO.gpio_function(5)
-func = GPIO.gpio_function(6)
-func = GPIO.gpio_function(7)
-func = GPIO.gpio_function(8)
-func = GPIO.gpio_function(9)
-func = GPIO.gpio_function(10)
-func = GPIO.gpio_function(11)
-func = GPIO.gpio_function(12)
-func = GPIO.gpio_function(13)
-func = GPIO.gpio_function(14)
-func = GPIO.gpio_function(15)
-func = GPIO.gpio_function(16)
-func = GPIO.gpio_function(17)
-func = GPIO.gpio_function(18)
-func = GPIO.gpio_function(19)
-func = GPIO.gpio_function(20)
-func = GPIO.gpio_function(21)
-func = GPIO.gpio_function(22)
-func = GPIO.gpio_function(23)
-func = GPIO.gpio_function(24)
-func = GPIO.gpio_function(25)
-func = GPIO.gpio_function(26)
-func = GPIO.gpio_function(27)
-func = GPIO.gpio_function(28)
-func = GPIO.gpio_function(29)
-func = GPIO.gpio_function(30)
-func = GPIO.gpio_function(31)
-func = GPIO.gpio_function(32)
-func = GPIO.gpio_function(33)
-func = GPIO.gpio_function(34)
-func = GPIO.gpio_function(35)
-func = GPIO.gpio_function(36)
-func = GPIO.gpio_function(37)
-func = GPIO.gpio_function(38)
-func = GPIO.gpio_function(39)
-func = GPIO.gpio_function(40)
-
-
+# demo of "gpio_function()" port test  
+# script by Alex Eames https://raspi.tv/?p=6805  
+  
+import RPi.GPIO as GPIO  
+  
+# Offer the user a choice of Pin or Port numbers and set numbering scheme accordingly  
+choice = raw_input("Type 1 for Pin numbers, anything else for BCM port numbers:\n")  
+if choice == "1":  
+    GPIO.setmode(GPIO.BOARD)  
+    ports = [3,5,7,8,10,11,12,13,15,16,18,19,21,22,23,24,26,29,31,32,33,35,36,37,38,40]  
+    pin_type = "Pin"  
+else:  
+    GPIO.setmode(GPIO.BCM)  
+    ports = [2,3,4,17,27,22,10,9,11,5,6,13,19,26,14,15,18,23,24,25,8,7,12,16,20,21]  
+    pin_type = "Port"  
+  
+print "%s mode selected..." % pin_type  
+  
+# Using a dictionary as a lookup table to give a name to gpio_function() return code  
+port_use = {0:"GPIO.OUT", 1:"GPIO.IN",40:"GPIO.SERIAL",41:"GPIO.SPI",42:"GPIO.I2C",  
+           43:"GPIO.HARD_PWM", -1:"GPIO.UNKNOWN"}  
+  
+# loop through the list of ports/pins querying and displaying the status of each  
+for port in ports:  
+    usage = GPIO.gpio_function(port)  
+    print "%s %d status: %s" % (pin_type, port, port_use[usage])  
