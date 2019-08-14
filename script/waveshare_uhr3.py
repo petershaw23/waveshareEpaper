216cfadd7ef3502ae7e36157ecc6e3f235afdd08
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 22:11:06 +0200

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -70,8 +70,8 @@ t = round(float(traw) / 1000)
 #try:
 data1 = requests.get(url="https://api.thingspeak.com/channels/647418/feeds.json?results=1")
 jsonobj1 = json.loads(data1.content.decode('utf-8'))
-tempPi1 = round(float(jsonobj1["feeds"][0]["field3"]), 2)
-humiPi1 = round(float(jsonobj1["feeds"][0]["field5"]), 2)
+tempPi1 = round(float(jsonobj1["feeds"][0]["field3"]), 1)
+humiPi1 = round(float(jsonobj1["feeds"][0]["field5"]), 1)
 #except:
 #tempPi1 = 'off'
 #humiPi1 = 'off'
@@ -80,15 +80,15 @@ humiPi1 = round(float(jsonobj1["feeds"][0]["field5"]), 2)
 time.sleep(2)
 data2 = requests.get(url="https://api.thingspeak.com/channels/843073/feeds.json?results=1")
 jsonobj2 = json.loads(data2.content.decode('utf-8'))
-tempD1 = round(float(jsonobj2["feeds"][0]["field1"]), 2)
-humiD1 = round(float(jsonobj2["feeds"][0]["field2"]), 2)
+tempD1 = round(float(jsonobj2["feeds"][0]["field1"]), 1)
+humiD1 = round(float(jsonobj2["feeds"][0]["field2"]), 1)
 #except:
 #    tempD1 = 'off'
 #    humiD1 = 'off'
 #calculate deltas
 try:
-    deltaT = round(float(tempPi1) - float(tempD1), 2)
-    deltaH = round(float(humiPi1) - float(humiD1), 2)
+    deltaT = round(float(tempPi1) - float(tempD1), 1)
+    deltaH = round(float(humiPi1) - float(humiD1), 1)
 except:
     deltaT = 'err'
     deltaH = 'err'
@@ -117,7 +117,7 @@ fontXL = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 33) 
 fontL = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 29) # font for bday1
 fontM = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 24) # font for volumio track ID
 fontS = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 21) # font for bday2
-fontXS = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 18) # font for temp, humi, cpu_temp
+fontXS = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 15) # font for temp, humi, cpu_temp
 ########################################################################################################
 ##############
 #draw function
@@ -146,7 +146,7 @@ def main():
         #draw.line((0, 77, 264, 77), fill = 0)
         draw.text((-4, 53), Uhrzeit, font = fontXXL, fill = 0)           # time
         draw.line((0, 160, 264, 160), fill = 0)
-        draw.text((0, 0), str(t),font = fontXS, fill = 0)
+        draw.text((70, 120), str(t),font = fontXS, fill = 0)
         draw.text((0, 159), 'in:'+str(tempPi1)+'°C|'+str(humiPi1)+str('%  out:')+str(tempD1)+'°C|'+str(humiD1)+str('%  Δt:' )+str(deltaT)+str('°C|ΔH:' )+str(deltaH)+str('%'), font = fontXS, fill = 0)       #temps
         
 
