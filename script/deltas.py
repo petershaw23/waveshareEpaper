468dd841bda3190807fa7dbc143e705871cb3c5f
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 14:17:56 +0200

    Update deltas.py

--- script/deltas.py
+++ script/deltas.py
@@ -1,26 +1,46 @@
-import thingspeak
+###
+# temperatur und humidity von thingspeak channel holen
 try:
     chPi1 = thingspeak.Channel(647418)
     outRAWPi1 = chPi1.get({'results':1})
-    outSplitPi1 = outRAWPi1.split('\"')
-    outTempPi1 = outSplitPi1[-18]
-    outHumiPi1 = outSplitPi1[-14]
-
     chD1 = thingspeak.Channel(843073)
     outRAWD1 = chD1.get({'results':1})
-    outSplitD1 = outRAWD1.split('\"')
-    outTempD1 = outSplitD1[-14]
-    outHumiD1 = outSplitD1[-10]
-    
-    deltaT = round(float(outTempPi1) - float(outTempD1), 3)
-    deltaH = round(float(outHumiPi1) - float(outHumiD1), 3)
-
 except: #falls offline
+    outTempPi1 = 'off'
+    outHumiPi1 = 'off'
+    outTempD1 = 'off'
+    outHumiD1 = 'off'
+    deltaT = 'off'
+    deltaH = 'off'    
+    
+outSplitPi1 = outRAWPi1.split('\"')
+try:
+    outTempPi1 = outSplitPi1[-18]
+except:
     outTempPi1 = 'err'
+    
+try:
+    outHumiPi1 = outSplitPi1[-14]
+except:
     outHumiPi1 = 'err'
+   
+outSplitD1 = outRAWD1.split('\"')
+    
+try:
+    outTempD1 = outSplitD1[-14]
+except:
     outTempD1 = 'err'
-    outHumiD1 = 'err'
+try:
+    outHumiD1 = outSplitD1[-10]
+except: 
+    outTempD1 = 'err'
+    
+try:
+    deltaT = round(float(outTempPi1) - float(outTempD1), 2)
+    deltaH = round(float(outHumiPi1) - float(outHumiD1), 2)
+except:
     deltaT = 'err'
     deltaH = 'err'
+
 
 print ('out: '+str(outTempD1)+'°C  '+str(outHumiD1)+str('%    in: ')+str(outTempPi1)+'°C  '+str(outHumiPi1)+str('%    Delta t: ' )+str(deltaT)+str('°C   Delta H: ' )+str(deltaH))
