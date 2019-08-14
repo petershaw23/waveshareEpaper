0467ae716c2ade1f7b2e77570f19500ecd0252ba
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 21:54:12 +0200

    Create clean.py

---
+++ script/clean.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+import sys
+sys.path.append(r'/home/pi/script/waveshareEpaper/lib')
+import epd2in7 #lib fuer display
+import epdconfig #config fuer display
+epd = epd2in7.EPD()
+epd.init()
+epd.Clear(0xFF)
+epd.sleep()
+GPIO.cleanup()
