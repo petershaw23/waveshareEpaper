51e15e43a48c65ac427ffc2d556f67c901ee5366
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 21:25:25 +0200

    Update button_test.py

--- script/button_test.py
+++ script/button_test.py
@@ -1,9 +1,41 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-def button_callback(channel):
-    print("Button was pushed!")
-#GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(5,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-message = input("Press enter to quit\n\n") # Run until someone presses enter
-GPIO.cleanup() # Clean up
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+key1 = 5
+key2 = 6
+key3 = 13
+key4 = 19
+
+GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+
+def main():
+
+    while True:
+        key1state = GPIO.input(key1)
+        key2state = GPIO.input(key2)
+        key3state = GPIO.input(key3)
+        key4state = GPIO.input(key4)
+
+        if key1state == False:
+            print('Key1 Pressed')
+            time.sleep(0.2)
+        if key2state == False:
+            print('Key2 Pressed')
+            time.sleep(0.2)
+        if key3state == False:
+            print('Key3 Pressed')
+            time.sleep(0.2)
+        if key4state == False:
+            print('Key4 Pressed')
+            updateDisplay('Key4 pressed')
+            time.sleep(0.2)
+
+if __name__ == '__main__':
+    main()
