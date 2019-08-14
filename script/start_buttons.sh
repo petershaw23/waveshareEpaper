98eea223ecaee74a22ce1ee99b171f3ed0ab8fdf
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 22:13:47 +0200

    Update start_buttons.sh

--- script/start_buttons.sh
+++ script/start_buttons.sh
@@ -2,7 +2,7 @@
 # u can call this file in crontab to execute the buttons script
 # like so: 
 # crontab -e 
-# * * * * * /home/pi/script/waveshareEpaper/script/start_buttons.sh >> /home/pi/script/waveshareEpaper/buttons.txt 2>&1
+# @reboot /home/pi/script/waveshareEpaper/script/start_buttons.sh >> /home/pi/script/waveshareEpaper/buttons.txt 2>&1
 
 #export DISPLAY=:0.0
 source /home/pi/.profile
