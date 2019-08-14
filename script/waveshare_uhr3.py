797b6ef4103ffe7b90b8f8eaf593a7919480a315
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 22:19:23 +0200

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -146,7 +146,7 @@ def main():
         #draw.line((0, 77, 264, 77), fill = 0)
         draw.text((-4, 53), Uhrzeit, font = fontXXL, fill = 0)           # time
         draw.line((0, 160, 264, 160), fill = 0)
-        draw.text((90, 110), str(t),font = fontXS, fill = 0)             #cpu temp
+        draw.text((120, 110), str(t),font = fontXS, fill = 0)             #cpu temp
         draw.text((0, 159), 'in:'+str(tempPi1)+'°|'+str(humiPi1)+str('%  out:')+str(tempD1)+'°|'+str(humiD1)+str('%  Δt:' )+str(deltaT)+str('°|ΔH:' )+str(deltaH)+str('%'), font = fontXS, fill = 0)       #temps
         
 
