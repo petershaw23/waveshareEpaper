f8ba328ac493303f041ad1e511510d8a9028ceb6
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-08-14 22:55:58 +0200

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -77,9 +77,9 @@ humiPi1 = round(float(jsonobj1["feeds"][0]["field5"]))
 #humiPi1 = 'off'
 # d1 mini data
 
-    time.sleep(2)
-    data2 = requests.get(url="https://api.thingspeak.com/channels/843073/feeds.json?results=1")
-    jsonobj2 = json.loads(data2.content.decode('utf-8'))
+#time.sleep(2)
+data2 = requests.get(url="https://api.thingspeak.com/channels/843073/feeds.json?results=1")
+jsonobj2 = json.loads(data2.content.decode('utf-8'))
 try:
     tempD1 = round(float(jsonobj2["feeds"][0]["field1"]))
     humiD1 = round(float(jsonobj2["feeds"][0]["field2"]))
