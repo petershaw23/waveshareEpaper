3dd350ffcc5eed9c1893dd147c37d70db0754e4c
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 09:19:03 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -3,3 +3,35 @@ import time
 List = open("geburtstage.txt").readlines()
 next_geb = List[0] #the first list entry
 print (next_geb)
+next_geb_dateRaw = (next_geb[0])
+next_geb_name = (next_geb[1])
+next_geb_date = datetime.datetime.strptime(next_geb_dateRaw, '%Y-%m-%d')
+deltaRawNext = next_geb_date - datetime.datetime.now()
+deltaNext = (deltaRawNext.days + 1)
+print (next_geb_dateRaw)
+print (next_geb_name)
+print (deltaRawNext)
+print (deltaNext)
+#            gebStringNext = ('t-' +str(deltaNext) +': ' +str(next_geb_name)) 
+#    except:
+#        gebStringNext = (' ')
+#    try:
+#        uebernext_geb = list[1] #the next after the first one
+#        uebernext_geb_dateRaw = (uebernext_geb[0])
+#        uebernext_geb_name = (uebernext_geb[1])
+#        uebernext_geb_date = datetime.datetime.strptime(uebernext_geb_dateRaw, '%Y-%m-%d')
+#        deltaRawUeberNext = uebernext_geb_date - datetime.datetime.now()
+#        deltaUeberNext = (deltaRawUeberNext.days + 1)
+#        if deltaUeberNext == 1:
+#            gebStringUeberNext = ('mrgn: '+str(uebernext_geb_name))
+#        else:
+#            gebStringUeberNext = ('t-'+str(deltaUeberNext)+': '+str(uebernext_geb_name))
+#    except:
+#        gebStringUeberNext = (' ')
+#
+#except: #falls fehler
+#    gebStringNext = '--'
+#    gebStringUeberNext = '--'
+#print (gebStringNext)
+#print (gebStringUeberNext)
+###
