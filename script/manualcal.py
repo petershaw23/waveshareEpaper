71d8f5a13c3c09cfbfa354e4d55cf2ced19e54f0
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 09:14:41 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -1,17 +1,5 @@
 #manual calendar, to not be reliant on google api
 import time 
-  
-birthdayFile = 'geburtstage.txt'
-  
-
-fileName = open(birthdayFile, 'r') 
-today = time.strftime('%m%d') 
-flag = 0
-for line in fileName: 
-    if today in line: 
-        line = line.split(' ') 
-        flag =1
-        # line[1] contains Name and line[2] contains Surname 
-        print("Birthdays Today: " + line[1])
-        if flag == 0: 
-            print("No Birthdays Today!") 
+List = open("geburtstage.txt").readlines()
+next_geb = List[0] #the first list entry
+print (next_geb)
