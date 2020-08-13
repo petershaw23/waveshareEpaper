0aec27e212e31cd5fb683b02a702f0abd6c82924
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 10:47:34 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -1,15 +1,4 @@
-import time
-#manual calendar, to not be reliant on google api
-fileName = open("geburtstage.txt", 'r') 
-today = time.strftime('%d.%m')
-flag = 0
-for line in fileName: 
-    if today in line: 
-        line = line.split(' ') 
-        flag = 1
-        
-        gebToday = line[1]
-        print("Birthdays Today: " + gebToday)
-    if flag == 0:
-        gebToday = "No Birthdays Today!"
-        print(gebToday) 
+with open('geburtstage.txt') as f:
+    lines = [line.rstrip() for line in f]
+    
+print (f)
