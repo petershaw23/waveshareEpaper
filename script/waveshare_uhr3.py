2ac59f4aa07b9d433fe6cabece7cea21edd1ae90
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 10:00:28 +0200

    Update waveshare_uhr3.py

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -22,8 +22,20 @@ Uhrzeit = datetime.datetime.now().strftime('%H:%M')
 print (Datum, Uhrzeit)
 
 
-import manualcal
-gebToday = gebToday.manualcal
+#manual calendar, to not be reliant on google api
+fileName = open("geburtstage.txt", 'r') 
+today = time.strftime('%d.%m')
+flag = 0
+for line in fileName: 
+    if today in line: 
+        line = line.split(' ') 
+        flag = 1
+        
+        gebToday = line[1]
+        print("Birthdays Today: " + gebToday)
+    if flag == 0:
+        gebToday = "No Birthdays Today!"
+        print(gebToday) 
     
     
 #    try:
