1bcb73743aaa8280ad6923259164200c2326010a
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 09:35:25 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -8,14 +8,7 @@ print (Datum, Uhrzeit)
 
 
 List = open('geburtstage.txt').read().split(',')
-next_geb = List[0] #the first list entry
-print (next_geb)
-next_geb_dateRaw = (next_geb[0])
-next_geb_name = (next_geb[1])
-#next_geb_date = datetime.datetime.strptime(next_geb_dateRaw, '%Y-%m-%d')
-#deltaRawNext = next_geb_date - datetime.datetime.now()
-#deltaNext = (deltaRawNext.days + 1)
-print (next_geb_dateRaw)
-print (next_geb_name)
-#print (deltaRawNext)
-#print (deltaNext)
+print (List[1])
+print (List[2])
+print (List[3])
+print (List[4])
