0d2a8c05222a1853a0108d6a489b51087aaccdeb
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 09:26:26 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -1,6 +1,7 @@
 #manual calendar, to not be reliant on google api
 import time 
 import datetime
+from dateutil import parser
 Datum = datetime.datetime.now().strftime('%-d.%-m.')
 Uhrzeit = datetime.datetime.now().strftime('%H:%M')
 print (Datum, Uhrzeit)
@@ -9,13 +10,13 @@ next_geb = List[0] #the first list entry
 print (next_geb)
 next_geb_dateRaw = (next_geb[0])
 next_geb_name = (next_geb[1])
-next_geb_date = datetime.datetime.strptime(next_geb_dateRaw, '%Y-%m-%d')
-deltaRawNext = next_geb_date - datetime.datetime.now()
-deltaNext = (deltaRawNext.days + 1)
+#next_geb_date = datetime.datetime.strptime(next_geb_dateRaw, '%Y-%m-%d')
+#deltaRawNext = next_geb_date - datetime.datetime.now()
+#deltaNext = (deltaRawNext.days + 1)
 print (next_geb_dateRaw)
 print (next_geb_name)
-print (deltaRawNext)
-print (deltaNext)
+#print (deltaRawNext)
+#print (deltaNext)
 #            gebStringNext = ('t-' +str(deltaNext) +': ' +str(next_geb_name)) 
 #    except:
 #        gebStringNext = (' ')
