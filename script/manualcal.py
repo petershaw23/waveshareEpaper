04150ecd9053ee669447b83bb90436aecc3028d8
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 09:08:34 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -3,18 +3,15 @@ import time
   
 birthdayFile = 'geburtstage.txt'
   
-def checkTodaysBirthdays(): 
-    fileName = open(birthdayFile, 'r') 
-    today = time.strftime('%m%d') 
-    flag = 0
-    for line in fileName: 
-        if today in line: 
-            line = line.split(' ') 
-            flag =1
-            # line[1] contains Name and line[2] contains Surname 
-            print("Birthdays Today: " + line[1]) 
-    if flag == 0: 
-            print("No Birthdays Today!") 
-  
-if __name__ == '__main__': 
-    checkTodaysBirthdays() 
+
+fileName = open(birthdayFile, 'r') 
+today = time.strftime('%m%d') 
+flag = 0
+for line in fileName: 
+    if today in line: 
+        line = line.split(' ') 
+        flag =1
+        # line[1] contains Name and line[2] contains Surname 
+        print("Birthdays Today: " + line[1])
+        if flag == 0: 
+        print("No Birthdays Today!") 
