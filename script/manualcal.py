78318b4ce195aeb800e415a030d1f0788da52f75
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 14:14:12 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -4,8 +4,12 @@ from datetime import datetime
 
 fileName = open("/home/pi/script/waveshareEpaper/script/geburtstage.txt", 'r') 
 today = datetime.now().strftime('%d.%m')
-tomorrowRaw = datetime.now() + timedelta(days=1) 
+
+tomorrowRaw = datetime.now() + timedelta(days=1)
 tomorrow = tomorrowRaw.strftime('%d.%m') 
+
+in2daysRaw = datetime.now() + timedelta(days=2)
+in2days = in2daysRaw.strftime('%d.%m') 
 
 flag = 0
 
@@ -20,9 +24,15 @@ for line in fileName:
         flag = 2
         gebTomorrow = line[1]
         print("Birthdays Tomorrow: " + gebTomorrow)
+    if in2days in line:
+        line = line.split(' ')
+        flag = 3
+        gebin2days = line[1]
+        print("Birthdays Tomorrow: " + gebin2days)
     if flag == 0:
         gebToday = " "
         gebTomorrow = " "
-        print(gebToday)
+        gebin2days = " "
+   
         
     
