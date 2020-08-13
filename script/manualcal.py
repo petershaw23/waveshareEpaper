3bdbf75074f045fa4e7ccc5ca6195fc30855ff07
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 14:22:11 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -14,35 +14,44 @@ in2days = in2daysRaw.strftime('%d.%m')
 in3daysRaw = datetime.now() + timedelta(days=3)
 in3days = in3daysRaw.strftime('%d.%m') 
 
-flag = 0
+in4daysRaw = datetime.now() + timedelta(days=4)
+in3days = in3daysRaw.strftime('%d.%m') 
+
+gebToday = " "
+gebTomorrow = " "
+gebin2days = " "
+gebin3days = " "
+gebin4days = " "
 
 for line in fileName: 
     if today in line: 
         line = line.split(' ') 
-        flag = 1
+   
         gebToday = line[1]
         print("Birthday Today: " + gebToday)
     if tomorrow in line:
         line = line.split(' ')
-        flag = 2
+     
         gebTomorrow = line[1]
         print("Birthday Tomorrow: " + gebTomorrow)
     if in2days in line:
         line = line.split(' ')
-        flag = 3
+
         gebin2days = line[1]
         print("Birthday in 2 days: " + gebin2days)
     if in3days in line:
         line = line.split(' ')
-        flag = 4
+
         gebin3days = line[1]
         print("Birthday in 3 days: " + gebin3days)
+    if in4days in line:
+        line = line.split(' ')
+
+        gebin4days = line[1]
+        print("Birthday in 3 days: " + gebin4days)
         
-    if flag == 0:
-        gebToday = " "
-        gebTomorrow = " "
-        gebin2days = " "
-        gebin3days = " "
+  
+
    
         
     
