0bce307d228cfb5a201a6c1fb124533ab892da65
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 10:55:22 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -4,4 +4,4 @@ with open('geburtstage.txt') as f:
     lines = [line.rstrip() for line in f]
     
 print (lines)
-dates_list = [dt.datetime.strptime(date, "%d.%m").date() for date in lines]
+dates_list = [datetime.datetime.strptime(date, "%d.%m").date() for date in lines]
