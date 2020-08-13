5c355f937b31f020c5de07476c073f5d6f8d2e16
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-13 13:49:06 +0200

    Update manualcal.py

--- script/manualcal.py
+++ script/manualcal.py
@@ -1,9 +1,17 @@
-from datetime import datetime
-
-with open('geburtstage.txt') as f:
-    lines = [line.rstrip() for line in f]
+#manual calendar, to not be reliant on google api
+fileName = open("/home/pi/script/waveshareEpaper/script/geburtstage.txt", 'r') 
+today = time.strftime('%d.%m')
+tomorrow = today + timedelta(days=1)  
+print (tomorrow)
+flag = 0
+for line in fileName: 
+    if today in line: 
+        line = line.split(' ') 
+        flag = 1
+        
+        gebToday = line[1]
+        print("Birthdays Today: " + gebToday)
+    if flag == 0:
+        gebToday = " "
+        print(gebToday) 
     
-print (lines)
-lines_split = lines.split()
-    
-dates_list = [datetime.strptime(date, "%d.%m").date() for date in lines_split]
