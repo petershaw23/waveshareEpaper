eda851e72fa4c9f4d06b76d535d4ee941765fb7c
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2026-07-07 10:56:14 +0200

    Update waveshare_uhr3.py
    noch mal vereinfacht

--- script/waveshare_uhr3.py
+++ script/waveshare_uhr3.py
@@ -1,170 +1,107 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-#uhr v3 by petershaw23 - shows time, date, current bday, current volumio song, CPU temp, temp+humidity via thingspeak channel
-print ('------------------------')
-#import datetime
-from datetime import timedelta
-from datetime import datetime
-import json
-import requests
-import http.client, urllib.parse
-import io
+# uhr v3 (vereinfacht) by petershaw23 - zeigt Zeit, Datum, anstehenden Geburtstag,
+# CPU-Temperatur sowie Temperatur/Luftfeuchte via ThingSpeak
+
 import sys
+import json
+from datetime import datetime, timedelta
+
+import requests
+from PIL import Image, ImageDraw, ImageFont
+
 sys.path.append(r'/home/pi/script/waveshareEpaper/lib')
-import epd2in7 #lib fuer display
-import epdconfig #config fuer display
-from PIL import Image,ImageDraw,ImageFont
-import subprocess, os
-from dateutil import parser
-import time
+import epd2in7  # lib fuer Display
 
-Datum = datetime.now().strftime('%-d.%-m.')
-Uhrzeit = datetime.now().strftime('%H:%M')
-print (Datum, Uhrzeit)
+LIB_PATH = '/home/pi/script/waveshareEpaper/lib'
+BDAY_FILE = '/home/pi/script/waveshareEpaper/script/geburtstage.txt'
+
+THINGSPEAK_PI1 = 'https://api.thingspeak.com/channels/843073/feeds.json?results=1'
+THINGSPEAK_D1 = 'https://api.thingspeak.com/channels/647418/feeds.json?results=1'
 
 
-#manual calendar, to not be reliant on google api
-fileName = open("/home/pi/script/waveshareEpaper/script/geburtstage.txt", 'r') 
-today = datetime.now().strftime('%d.%m')
-tomorrowRaw = datetime.now() + timedelta(days=1)
-tomorrow = tomorrowRaw.strftime('%d.%m') 
-in2daysRaw = datetime.now() + timedelta(days=2)
-in2days = in2daysRaw.strftime('%d.%m') 
-in3daysRaw = datetime.now() + timedelta(days=3)
-in3days = in3daysRaw.strftime('%d.%m') 
-in4daysRaw = datetime.now() + timedelta(days=4)
-in4days = in4daysRaw.strftime('%d.%m') 
-countdown = ""
-geb = ""
-count = 0
+def get_naechster_geburtstag(pfad):
+    """Sucht die naechsten 0-4 Tage in geburtstage.txt nach einem Treffer."""
+    labels = ["heute: ", "mrgn: ", "t-2: ", "t-3: ", "t-4: "]
+    daten = [(datetime.now() + timedelta(days=i)).strftime('%d.%m') for i in range(5)]
 
-for line in fileName: 
-    if today in line and count == 0: 
-        line = line.split(' ') 
-        geb = line[1]
-        countdown = "heute: "
-        print(str(countdown) + str(geb))
-        count +=1
-    if tomorrow in line and count == 0:
-        line = line.split(' ')
-        geb = line[1]
-        countdown = "mrgn: "
-        print(str(countdown) + str(geb))
-        count +=1
-    if in2days in line and count == 0:
-        line = line.split(' ')
-        geb = line[1]
-        countdown = "t-2: "
-        print(str(countdown) + str(geb))
-        count +=1
-    if in3days in line and count == 0:
-        line = line.split(' ')
-        geb = line[1]
-        countdown = "t-3: "
-        print(str(countdown) + str(geb))
-        count +=1
-    if in4days in line and count == 0:
-        line = line.split(' ')
-        geb = line[1]
-        countdown = "t-4: "
-        print(str(countdown) + str(geb))
-        count +=1
-    
+    with open(pfad, 'r') as f:
+        zeilen = f.readlines()
+
+    for tag_str, label in zip(daten, labels):
+        for zeile in zeilen:
+            if tag_str in zeile:
+                name = zeile.split(' ')[1].strip()
+                return label, name
+    return "", ""
 
 
-f = open("/sys/class/thermal/thermal_zone0/temp", "r") #raspberry pi CPU temp
-traw = f.readline ()
-t = round(float(traw) / 1000)
+def get_cpu_temp():
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        return round(float(f.readline()) / 1000)
 
 
-# temperatur und humidity von thingspeak channel holen
-# pi1 data
-data1 = requests.get(url="https://api.thingspeak.com/channels/843073/feeds.json?results=1")
-jsonobj1 = json.loads(data1.content.decode('utf-8'))
-tempPi1 = round(float(jsonobj1["feeds"][0]["field2"]))
-#humiPi1 = round(float(jsonobj1["feeds"][0]["field5"]))
-humiPi1 = 50
-# d1 mini data
-data2 = requests.get(url="https://api.thingspeak.com/channels/647418/feeds.json?results=1")
-jsonobj2 = json.loads(data2.content.decode('utf-8'))
-try:
-    tempD1 = round(float(jsonobj2["feeds"][0]["field2"]))
-    #humiD1 = round(float(jsonobj2["feeds"][0]["field2"]))
-    humiD1 = 50
-    last_entry_D1 = jsonobj2["feeds"][0]["created_at"] #time of last entry
-except: #if entry is Null
-    tempD1 = jsonobj2["feeds"][0]["field1"] #displays the field entry (e.g. null)
-    humiD1 = jsonobj2["feeds"][0]["field2"] #displays the field entry (e.g. null)
-    last_entry_D1 = jsonobj2["feeds"][0]["created_at"] #same as in try
+def get_thingspeak_temp_humi(url, humi_fallback=50):
+    """Holt Temperatur (+ Platzhalter-Luftfeuchte) von einem ThingSpeak-Channel."""
+    data = requests.get(url=url).json()
+    feed = data["feeds"][0]
+    try:
+        temp = round(float(feed["field2"]))
+        humi = humi_fallback
+    except (TypeError, ValueError):
+        temp = feed.get("field1")
+        humi = feed.get("field2")
+    return temp, humi
 
-#calculate deltaT and deltaH
-try:
-    deltaT = round(float(tempPi1) - float(tempD1))
-    deltaH = round(float(humiPi1) - float(humiD1))
-except:
-    deltaT = 'err'
-    deltaH = 'err'
-print (str(t)+'°C   in: '+str(tempPi1)+'°C  '+str(humiPi1)+str('%    out: ')+str(tempD1)+'°C  '+str(humiD1)+str('%    Δt: ' )+str(deltaT)+str('°C   ΔH: ' )+str(deltaH))
-    
-# track ID via volumio REST api holen:
 
-#trackid = subprocess.Popen("curl 192.168.0.164/api/v1/getstate", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-#(outputRAW, error) = trackid.communicate()
-#if trackid.returncode != 0: #if offline
-#   artist = ' '
-#   trackname = ' '
-   #trackIDString = '        Volumio Offline' # placeholder for test 
- #  trackIDString = '- - - - - - - - - - - - - - - - - - - - -'
-#else:
-#   trackname = outputRAW.decode().split('\"')[9]
-#   artist = outputRAW.decode().split('\"')[13]
-#   trackIDString = (str(artist)+str(' - ')+str(trackname))
-   #albumart = outputRAW.decode().split('\"')[21] #das waere sau cool
+def get_delta(a, b):
+    try:
+        return round(float(a) - float(b))
+    except (TypeError, ValueError):
+        return 'err'
 
-#print (trackIDString)
-######################################################################################################
-#schriftarten definieren
-fontXXL = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 107) # font for time
-fontXL = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 32) # font for date
-fontL = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 28) # font for bday1
-fontL2 = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 29) # font for new temps
-fontM = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 24) # font for volumio track ID
-fontS = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 21) # font for bday2
-fontXS = ImageFont.truetype('/home/pi/script/waveshareEpaper/lib/Font.ttc', 17) # font for temp, humi, cpu_temp
-########################################################################################################
-##############
-#draw function
-##############
+
 def main():
-        #Init driver
-        epd = epd2in7.EPD()
-        epd.init()
-        # Image with screen size
-        #255: clear the image with white
-        image = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)
-        #Object image on which we will draw
-        draw = ImageDraw.Draw(image)
-        
-        
-        #draw.rectangle((0, 0, 264, 49), fill = 0) #rectangle behind bdays and date
-        #draw.rectangle((0, 48, 264, 71), fill = 0) #rectangle behind track ID
-        draw.text((0, -4), str(Datum)+str(' ')+str(countdown)+str(geb), font = fontXL, fill = 0)              # Date + next bday
-        draw.line((5, 33, 259, 33), fill = 0)
-        #draw.text((0, 45), trackIDString, font = fontM, fill = 1)       # volumio track ID
-        #draw.text((-4, 53), Uhrzeit, font = fontXXL, fill = 0)           # time alte version unter track ID
-        draw.text((-4, 16), Uhrzeit, font = fontXXL, fill = 0)
-        draw.line((0, 125, 264, 125), fill = 0)
-        draw.text((120, 80), str(t),font = fontXS, fill = 0)             #cpu temp   
-        #draw.text((0, 159), 'i:'+str(tempPi1)+'°|'+str(humiPi1)+str('%  o:')+str(tempD1)+'°|'+str(humiD1)+str('%  Δt:' )+str(deltaT)+str('°|ΔH:' )+str(deltaH)+str('%'), font = fontXS, fill = 0)       #temps alte version bei nutzung der Track ID
-        draw.text((0, 130), 'i:'+str(tempPi1)+'°|'+str(humiPi1)+str('% o:')+str(tempD1)+'°|'+str(humiD1), font = fontL2, fill = 0)       #temps
-        
+    datum = datetime.now().strftime('%-d.%-m.')
+    uhrzeit = datetime.now().strftime('%H:%M')
 
-        #Update display
-        epd.display(epd.getbuffer(image))
-        #sleep display
-        epd.sleep()
- 
+    countdown, geb = get_naechster_geburtstag(BDAY_FILE)
+    cpu_temp = get_cpu_temp()
+
+    temp_innen, humi_innen = get_thingspeak_temp_humi(THINGSPEAK_PI1)
+    temp_aussen, humi_aussen = get_thingspeak_temp_humi(THINGSPEAK_D1)
+
+    delta_t = get_delta(temp_innen, temp_aussen)
+    delta_h = get_delta(humi_innen, humi_aussen)
+
+    print(f"{datum} {uhrzeit}")
+    print(f"{countdown}{geb}")
+    print(f"{cpu_temp}°C in: {temp_innen}°C {humi_innen}% "
+          f"out: {temp_aussen}°C {humi_aussen}% Δt: {delta_t}°C ΔH: {delta_h}")
+
+    # Schriftarten
+    font_xxl = ImageFont.truetype(f'{LIB_PATH}/Font.ttc', 107)  # Uhrzeit
+    font_xl = ImageFont.truetype(f'{LIB_PATH}/Font.ttc', 32)    # Datum
+    font_xs = ImageFont.truetype(f'{LIB_PATH}/Font.ttc', 17)    # CPU-Temp
+    font_l2 = ImageFont.truetype(f'{LIB_PATH}/Font.ttc', 29)    # Temp/Humi
+
+    epd = epd2in7.EPD()
+    epd.init()
+
+    image = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)
+    draw = ImageDraw.Draw(image)
+
+    draw.text((0, -4), f"{datum} {countdown}{geb}", font=font_xl, fill=0)
+    draw.line((5, 33, 259, 33), fill=0)
+    draw.text((-4, 16), uhrzeit, font=font_xxl, fill=0)
+    draw.line((0, 125, 264, 125), fill=0)
+    draw.text((120, 80), str(cpu_temp), font=font_xs, fill=0)
+    draw.text((0, 130), f"i:{temp_innen}°|{humi_innen}% o:{temp_aussen}°|{humi_aussen}",
+               font=font_l2, fill=0)
+
+    epd.display(epd.getbuffer(image))
+    epd.sleep()
+
 
 if __name__ == '__main__':
     main()
-
